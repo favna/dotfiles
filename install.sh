108533256ffb06d879ef434644cb0a79d9591c86
Jeroen Claassens <support@favware.tech>
2020-08-28 19:25:45 +0200

    chore: this bunny should be killed.

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Write all files to $HOME
-cat ./.bashrc > $HOME/.bashrc
-cat ./.p10k.zsh > $HOME/.p10k.zsh
-cat ./.profile > $HOME/.profile
-cat ./.zshrc > $HOME/.zshrc
+# Write all files to ~/
+cat ./.bashrc > ~/.bashrc
+cat ./.p10k.zsh > ~/.p10k.zsh
+cat ./.profile > ~/.profile
+cat ./.zshrc > ~/.zshrc
 
 # Download oh-my-zsh
 # git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
@@ -16,8 +16,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/p
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k --depth=1
 
 # Install Meslo NGF font
-mkdir $HOME/.fonts
-curl -sLo $HOME/.fonts/MesloLGS\ NF\ Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-curl -sLo $HOME/.fonts/MesloLGS\ NF\ Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-curl -sLo $HOME/.fonts/MesloLGS\ NF\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-curl -sLo $HOME/.fonts/MesloLGS\ NF\ Bold\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+mkdir ~/.fonts
+curl -sLo ~/.fonts/MesloLGS\ NF\ Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+curl -sLo ~/.fonts/MesloLGS\ NF\ Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+curl -sLo ~/.fonts/MesloLGS\ NF\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+curl -sLo ~/.fonts/MesloLGS\ NF\ Bold\ Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
