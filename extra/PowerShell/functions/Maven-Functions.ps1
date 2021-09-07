2eecc5884156a4af264e18d80c2d7da2eaadfe61
Jeroen Claassens <j.claassens@cgi.com>
2021-09-07 15:59:48 +0200

    fix: update maven functions

--- extra/PowerShell/functions/Maven-Functions.ps1
+++ extra/PowerShell/functions/Maven-Functions.ps1
@@ -1,5 +1,5 @@
 Function maven {
-    mvn -T 4.0C -pl !gms/gms-release -pl !sbc/sbc-release -pl !wli/wli-release -DskipTests @Args
+    mvn -T 4.0C -pl !gms/gms-release -pl !sbc/sbc-release -pl !wli/wli-release -DskipTests -D"maven.javadoc.skip"=true -DskipRpms -D"skip.keten.tests"=true -D"skip.integration.tests"=false @Args
 }
 
 Function mvnci {
