f06156a348cfa6ab81d179ae2eb6af04f168e6a1
Jeroen Claassens <j.claassens@cgi.com>
2021-09-08 09:23:13 +0200

    fix: run-script positional parameters

--- extra/PowerShell/functions/Laptop-Functions.ps1
+++ extra/PowerShell/functions/Laptop-Functions.ps1
@@ -4,33 +4,37 @@ Function Run-Script {
         [Parameter(Position = 0, Mandatory = $True)]
         [ValidateNotNull()]
         [string]
-        $Script
+        $Script,
+
+        [Parameter(Position = 1, Mandatory = $False, ValueFromRemainingArguments = $True)]
+        [string[]]
+        $Flags
     )
 
     Process {
-        $scriptDirectory = "C:\Users\j.claassens\Documents\workspace\scripts"
+        $scriptDirectory = 'C:\Users\j.claassens\Documents\workspace\scripts'
 
         switch -regex ($Script) {
             build {
-                & $scriptDirectory\Build-Frontend @Args
+                & $scriptDirectory\Build-Frontend.ps1 $Flags
             }
             package {
-                & $scriptDirectory\Build-Backend @Args
+                & $scriptDirectory\Build-Backend.ps1 $Flags
             }
             wars {
-                & $scriptDirectory\Deploy-Wars @Args
+                & $scriptDirectory\Deploy-Wars.ps1 $Flags
             }
             jboss {
-                & $scriptDirectory\Start-Jboss @Args
+                & $scriptDirectory\Start-Jboss.ps1 $Flags
             }
-            "^(?:logs|clearlogs|clear-logs)" {
-                & $scriptDirectory\Clear-Logs @Args
+            '^(?:logs|clearlogs|clear-logs)' {
+                & $scriptDirectory\Clear-Logs.ps1 $Flags
             }
             console {
-                & $scriptDirectory\Start-Jboss-Console @Args
+                & $scriptDirectory\Start-Jboss-Console.ps1 $Flags
             }
             Default {
-                Write-Host "Unknown script parameter detected" -ForegroundColor Red
+                Write-Host 'Unknown script parameter detected' -ForegroundColor Red
             }
         }
     }
