9d618c9f3deb23db6c4d1005c9a70f35ee73e3d0
Jeroen Claassens <j.claassens@cgi.com>
2021-09-09 10:51:09 +0200

    feat: add mvnb and mvnbc functions

--- extra/PowerShell/functions/Maven-Functions.ps1
+++ extra/PowerShell/functions/Maven-Functions.ps1
@@ -13,3 +13,11 @@ Function mvni {
 Function mvnc {
     maven clean
 }
+
+Function mvnb {
+    mvn install -T 4.0C -DskipTests -D"maven.javadoc.skip" -DskipRpms -D"skip.keten.tests" -D"skip.integration.tests" @Args
+}
+
+Function mvnbc {
+    mvn clean install -T 4.0C -DskipTests -D"maven.javadoc.skip" -DskipRpms -D"skip.keten.tests" -D"skip.integration.tests" @Args
+}
