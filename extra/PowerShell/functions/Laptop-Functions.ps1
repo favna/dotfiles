ce7369c850a72b284b686479f5ff2d2076b8525e
Jeroen Claassens <j.claassens@cgi.com>
2021-09-13 15:19:21 +0200

    feat: intelligently select monitor to change reso for

--- extra/PowerShell/functions/Laptop-Functions.ps1
+++ extra/PowerShell/functions/Laptop-Functions.ps1
@@ -71,9 +71,17 @@ Function Build-Trigger {
 
 
 Function Set-Sharing-Resolution {
-    Set-ScreenResolutionEx 1920 1080 1
+    $ChangeDisplay1Result = Set-ScreenResolutionEx 1920 1080 1
+
+    if ($ChangeDisplay1Result -eq 'Failed To Change The Resolution.') {
+        Set-ScreenResolutionEx 1920 1080 0
+    }
 }
 
 Function Set-Working-Resolution {
-    Set-ScreenResolutionEx 3440 1440 1
+    $ChangeDisplay1Result = Set-ScreenResolutionEx 3440 1440 1
+
+    if ($ChangeDisplay1Result -eq 'Failed To Change The Resolution.') {
+        Set-ScreenResolutionEx 3440 1440 0
+    }
 }
