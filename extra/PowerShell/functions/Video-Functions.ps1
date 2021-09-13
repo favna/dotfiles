d19511b034455837b6d3248e3e0426e8bebe9bd2
Jeroen Claassens <support@favware.tech>
2021-09-13 22:30:57 +0200

    feat: add Cut-Video-Copy

--- extra/PowerShell/functions/Video-Functions.ps1
+++ extra/PowerShell/functions/Video-Functions.ps1
@@ -2,6 +2,10 @@ Function Cut-Video {
 	ffmpeg -i $args[0] -ss $args[1] -to $args[2] -y .\out.mp4
 }
 
+Function Cut-Video-Copy {
+	ffmpeg -i $args[0] -ss $args[1] -to $args[2] -c:v copy -c:a copy -y .\out.mp4
+}
+
 Function Compress-Video {
 	ffmpeg -i $args[0] -b:v 12800k -c:a copy -y .\out.mp4
 }
