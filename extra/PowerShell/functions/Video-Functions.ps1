c50aee6b051abf22438c0f2f061b3f8832690557
Jeroen Claassens <support@favware.tech>
2022-03-08 10:48:18 +0100

    feat: add convert-mkv

--- extra/PowerShell/functions/Video-Functions.ps1
+++ extra/PowerShell/functions/Video-Functions.ps1
@@ -10,6 +10,10 @@ Function Compress-Video {
 	ffmpeg -i $args[0] -b:v 12800k -c:a copy -y .\out.mp4
 }
 
+Function Convert-Mkv {
+	ffmpeg -i $args[0] -c copy -y .\out.mp4
+}
+
 Function Launch-Switch-Dvr {
 	dotnet 'E:\ProgramFiles\SysDVR-Client\SysDVR-Client.dll' bridge 192.168.2.2
 }
