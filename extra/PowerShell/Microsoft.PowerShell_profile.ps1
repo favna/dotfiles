df049915fee0708e3b43d9d12a2e11a57360f65c
Jeroen Claassens <support@favware.tech>
2022-04-10 23:30:26 +0200

    chore: add git cliff completions

--- extra/PowerShell/Microsoft.PowerShell_profile.ps1
+++ extra/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -37,3 +37,43 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 $Env:STARSHIP_CONFIG = "$PSScriptRoot\starship.toml"
 Invoke-Expression (&starship init powershell)
+$functionsDirectory = "$PSScriptRoot\functions";
+$completionsDirectory = "$PSScriptRoot\completions";
+$aliasesDirectory = "$PSScriptRoot\aliases";
+
+Import-Module -Name posh-git
+Import-Module -Name Terminal-Icons
+Import-Module -Name PSReadLine
+
+$PSReadLineOptions = @{
+    PredictionSource = 'History'
+    Colors           = @{ 
+        'Command' = '#8181f7'
+        'Comment' = 'DarkGray'
+    }
+}
+
+Set-PSReadLineOption @PSReadLineOptions
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Load all custom functions
+. $functionsDirectory\Git-Functions.ps1
+. $functionsDirectory\GitHub-Cli-Functions.ps1
+. $functionsDirectory\NodeJS-Functions.ps1
+. $functionsDirectory\Terminal-Functions.ps1
+. $functionsDirectory\Video-Functions.ps1
+. $functionsDirectory\ScreenResolution.ps1
+
+# Load all custom completions
+& "$completionsDirectory\Starship-Completions.ps1"
+& "$completionsDirectory\Gh-Completions.ps1"
+& "$completionsDirectory\Custom-Completions.ps1"
+& "$completionsDirectory\Volta-Completions.ps1"
+& "$completionsDirectory\Git-Cliff-Completions.ps1"
+
+# Load all custom aliases
+. $aliasesDirectory\Custom-Aliases.ps1
+
+$Env:STARSHIP_CONFIG = "$PSScriptRoot\starship.toml"
+Invoke-Expression (&starship init powershell)
---
+++ extra/PowerShell/completions/Git-Cliff-Completions.ps1
@@ -0,0 +1,64 @@
+
+using namespace System.Management.Automation
+using namespace System.Management.Automation.Language
+
+Register-ArgumentCompleter -Native -CommandName 'git-cliff' -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+
+    $commandElements = $commandAst.CommandElements
+    $command = @(
+        'git-cliff'
+        for ($i = 1; $i -lt $commandElements.Count; $i++) {
+            $element = $commandElements[$i]
+            if ($element -isnot [StringConstantExpressionAst] -or
+                $element.StringConstantType -ne [StringConstantType]::BareWord -or
+                $element.Value.StartsWith('-') -or
+                $element.Value -eq $wordToComplete) {
+                break
+        }
+        $element.Value
+    }) -join ';'
+
+    $completions = @(switch ($command) {
+        'git-cliff' {
+            [CompletionResult]::new('-c', 'c', [CompletionResultType]::ParameterName, 'Sets the configuration file')
+            [CompletionResult]::new('--config', 'config', [CompletionResultType]::ParameterName, 'Sets the configuration file')
+            [CompletionResult]::new('-w', 'w', [CompletionResultType]::ParameterName, 'Sets the working directory')
+            [CompletionResult]::new('--workdir', 'workdir', [CompletionResultType]::ParameterName, 'Sets the working directory')
+            [CompletionResult]::new('-r', 'r', [CompletionResultType]::ParameterName, 'Sets the git repository')
+            [CompletionResult]::new('--repository', 'repository', [CompletionResultType]::ParameterName, 'Sets the git repository')
+            [CompletionResult]::new('--include-path', 'include-path', [CompletionResultType]::ParameterName, 'Sets the path to include related commits')
+            [CompletionResult]::new('--exclude-path', 'exclude-path', [CompletionResultType]::ParameterName, 'Sets the path to exclude related commits')
+            [CompletionResult]::new('--with-commit', 'with-commit', [CompletionResultType]::ParameterName, 'Sets custom commit messages to include in the changelog')
+            [CompletionResult]::new('-p', 'p', [CompletionResultType]::ParameterName, 'Prepends entries to the given changelog file')
+            [CompletionResult]::new('--prepend', 'prepend', [CompletionResultType]::ParameterName, 'Prepends entries to the given changelog file')
+            [CompletionResult]::new('-o', 'o', [CompletionResultType]::ParameterName, 'Writes output to the given file')
+            [CompletionResult]::new('--output', 'output', [CompletionResultType]::ParameterName, 'Writes output to the given file')
+            [CompletionResult]::new('-t', 't', [CompletionResultType]::ParameterName, 'Sets the tag for the latest version')
+            [CompletionResult]::new('--tag', 'tag', [CompletionResultType]::ParameterName, 'Sets the tag for the latest version')
+            [CompletionResult]::new('-b', 'b', [CompletionResultType]::ParameterName, 'Sets the template for the changelog body')
+            [CompletionResult]::new('--body', 'body', [CompletionResultType]::ParameterName, 'Sets the template for the changelog body')
+            [CompletionResult]::new('-s', 's', [CompletionResultType]::ParameterName, 'Strips the given parts from the changelog')
+            [CompletionResult]::new('--strip', 'strip', [CompletionResultType]::ParameterName, 'Strips the given parts from the changelog')
+            [CompletionResult]::new('--sort', 'sort', [CompletionResultType]::ParameterName, 'Sets sorting of the commits inside sections')
+            [CompletionResult]::new('-v', 'v', [CompletionResultType]::ParameterName, 'Increases the logging verbosity')
+            [CompletionResult]::new('--verbose', 'verbose', [CompletionResultType]::ParameterName, 'Increases the logging verbosity')
+            [CompletionResult]::new('-i', 'i', [CompletionResultType]::ParameterName, 'Writes the default configuration file to cliff.toml')
+            [CompletionResult]::new('--init', 'init', [CompletionResultType]::ParameterName, 'Writes the default configuration file to cliff.toml')
+            [CompletionResult]::new('-l', 'l', [CompletionResultType]::ParameterName, 'Processes the commits starting from the latest tag')
+            [CompletionResult]::new('--latest', 'latest', [CompletionResultType]::ParameterName, 'Processes the commits starting from the latest tag')
+            [CompletionResult]::new('--current', 'current', [CompletionResultType]::ParameterName, 'Processes the commits that belong to the current tag')
+            [CompletionResult]::new('-u', 'u', [CompletionResultType]::ParameterName, 'Processes the commits that do not belong to a tag')
+            [CompletionResult]::new('--unreleased', 'unreleased', [CompletionResultType]::ParameterName, 'Processes the commits that do not belong to a tag')
+            [CompletionResult]::new('--date-order', 'date-order', [CompletionResultType]::ParameterName, 'Sorts the tags chronologically')
+            [CompletionResult]::new('-h', 'h', [CompletionResultType]::ParameterName, 'Prints help information')
+            [CompletionResult]::new('--help', 'help', [CompletionResultType]::ParameterName, 'Prints help information')
+            [CompletionResult]::new('-V', 'V', [CompletionResultType]::ParameterName, 'Prints version information')
+            [CompletionResult]::new('--version', 'version', [CompletionResultType]::ParameterName, 'Prints version information')
+            break
+        }
+    })
+
+    $completions.Where{ $_.CompletionText -like "$wordToComplete*" } |
+        Sort-Object -Property ListItemText
+}
