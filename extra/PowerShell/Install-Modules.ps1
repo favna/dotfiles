f40925710b038e7dff6eb8adbd16f6513b942b50
Jeroen Claassens <support@favware.tech>
2024-05-03 22:50:29 +0200

    feat(pwsh): add script to install all required modules

---
+++ extra/PowerShell/Install-Modules.ps1
@@ -0,0 +1,5 @@
+Install-Module -Repository PSGallery -Scope CurrentUser -Force -AllowClobber -AcceptLicense -Name git-aliases
+Install-Module -Repository PSGallery -Scope CurrentUser -Force -AllowClobber -AcceptLicense -Name posh-git
+Install-Module -Repository PSGallery -Scope CurrentUser -Force -AllowClobber -AcceptLicense -Name Terminal-Icons
+Install-Module -Repository PSGallery -Scope CurrentUser -Force -AllowClobber -AcceptLicense -Name PSFzf
+Install-Module -Repository PSGallery -Scope CurrentUser -Force -AllowClobber -AcceptLicense -Name z
